4525b6c17ad609853f150b28bc8ec6da148b1542
Ailton de Souza Junior <ailton.souza97@hotmail.com>
2022-03-02 19:23:21 -0300

    Password commit

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://ailtonjr:n21gwCXk4pgNZDLT@cluster0.dq7kh.mongodb.net/todolistDB", { useNewUrlParser: true });
+mongoose.connect("mongodb+srv://ailtonjr:<>.dq7kh.mongodb.net/todolistDB", { useNewUrlParser: true });
 
 const itemsSchema = {
   name: String
